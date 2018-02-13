6c9088229ef908c12f87abb811526d033b602686
Lythi45 <scala.bine@gmail.com>
2018-02-13 17:36:09 +0100

    reflection rays

--- reflektion/script.js
+++ reflektion/script.js
@@ -7,53 +7,59 @@ function setup() {
   var p4 = p3+d;
 
   // Sets the screen to be 720 pixels wide and 400 pixels high
-  createCanvas(720, 400);
+  createCanvas(800, 400);
   background(0);
   noSmooth();
 
-    input = createInput('test');
+    input = createInput('sin(x)');
     input.position(20, 65);
 
     button = createButton('submit');
     button.position(input.x + input.width, 65);
     button.mousePressed(greet);
 
-    greeting = createElement('h2', 'what is your name?');
+    greeting = createElement('h2', 'Funktion mit x eingeben:');
     greeting.position(20, 5);
 
     textAlign(CENTER);
-    textSize(50);
+    textSize(0.5);
 
-  translate(140, 0);
 
-  // Draw gray box
-  stroke(153);
-  line(p3, p3, p2, p3);
-  line(p2, p3, p2, p2);
-  line(p2, p2, p3, p2);
-  line(p3, p2, p3, p3);
-
-  // Draw white points
-  stroke(255);
-  point(p1, p1);
-  point(p1, p3);
-  point(p2, p4);
-  point(p3, p1);
-  point(p4, p2);
-  point(p4, p4);
 }
 
 function greet() {
+  xf=100;yf=150;xs=400;ys=200;
+  tiny=0.00001;
   var name = input.value();
-  greeting.html('hello '+name+'!');
+  //greeting.html('hello '+name+'!');
   input.value('');
-
-  for (var i=0; i<200; i++) {
-    push();
-    fill(random(255), 255, 255);
-    translate(random(width), random(height));
-    rotate(random(2*PI));
-    text(name, 0, 0);
-    pop();
+background(0);
+push();
+translate(xs,ys);
+scale(xf,-yf);
+strokeWeight(0.01);
+  for (var x=-4; x<4; x+=0.01) {
+    y=eval(name);
+stroke(255);
+    if (x>-4) {
+      line(ax,ay,x,y);
+    }
+    ay=y;
+    ax=x;
   }
+
+  stroke(255,0,0);
+  for (var xx=-4;xx<4;xx+=0.2){
+    x=xx;
+    y=eval(name);
+    x=xx+tiny;
+    y2=eval(name);
+    winkel=atan((y2-y)/tiny)*2+Math.PI/2;
+    wx=cos(winkel)*3;
+    wy=sin(winkel)*3;
+    line(x,3,x,y);
+    line(x,y,x+wx,y+wy);
+    print(x+" "+y+" "+winkel+" "+wx+" "+wy);
+  }
+  pop();
 }
